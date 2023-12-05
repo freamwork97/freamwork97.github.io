f25a4aa1aa2856dc587905000c02bb0302fa4d27
freamwork97 <jsy11123@naver.com>
2023-12-05 16:16:05 +0900

    뉴스 추가

--- JS/ITNews.js
+++ JS/ITNews.js
@@ -15,6 +15,8 @@ $(function(){
         '<a href="https://www.ciokorea.com/news/315359">노키아, 인간의 음성으로 네트워크 재구성하는 인공지능 기술 공개</a>',
         '<a href="https://n.news.naver.com/mnews/article/016/0002233796?sid=105">[IT과학칼럼] 6G 선점 위한 저궤도 위성통신의 중요성</a>',
         '<a href="https://www.psnews.co.kr/news/articleView.html?idxno=2040327">SK, 대구에 8천억원 규모 AI 데이터센터 투자</a>',
+        '<a href="http://www.digitaltoday.co.kr/news/articleView.html?idxno=496979">구글 딥마인드, \'모방 학습\'으로 AI 기술 향상…연구</a>',
+        '<a href="http://www.digitaltoday.co.kr/news/articleView.html?idxno=496983">메타-IBM 주도 AI 얼라이언스 뜬다...AI기업·연구기관 50여곳 참여</a>',
         // Add your links here
     ];
 
