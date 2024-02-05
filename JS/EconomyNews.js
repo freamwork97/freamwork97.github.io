7343bfd064efb71317ce6a35f1549d2ff19a59b8
freamwork97 <jsy11123@naver.com>
2024-02-05 12:43:12 +0900

    뉴스 추가

--- JS/EconomyNews.js
+++ JS/EconomyNews.js
@@ -38,6 +38,7 @@ $(function () {
     "<a href='https://kr.investing.com/news/stock-market-news/article-993330'>상장사, 작년 자사주 4.8조 태웠다…신한지주·현대차 1·2위</a>",
     "<a href='https://n.news.naver.com/mnews/article/001/0014478384?rc=N&ntype=RANKING'>삼성전자 \"2026년까지 3년간 잉여현금흐름 50% 주주환원\"</a>",
     "<a href='https://kr.investing.com/news/stock-market-news/article-997278'>‘1조 주식 소각’ 삼성물산, 수익성·주주가치 제고 두 마리 토끼 모두 잡는다</a>",
+    "<a href='https://www.citytimes.co.kr/news/articleView.html?idxno=31722'>'무역전쟁' 예고한 트럼프…\"재집권시 대중관세 60%\"</a>",
     // Add your links here
   ];
 
